f35f57a6b32cfad0c02fc6d9dddcf389c93d25a0
mskelton <mskelton@users.noreply.github.com>
2020-11-09 14:38:19 +0000

    Update build artifact

--- dist/index.js
+++ dist/index.js
@@ -357,6 +357,7 @@ const messages = {
     commentMinimized: 'Looks like there are no longer any diffs, so I went ahead and resolved the outdated comment.',
     commentUpdated: 'I found some new diffs since the last time I checked. Take a look at the comment to see what changed.',
     noDiffs: "Good news! I didn't find any diffs so everything in Locize is up to date!",
+    diffsUnchanged: `Looks like diffs haven't changed since I last checked. If you want to copy the diffs from left to right, try running \`@locize-diff copy\`.`,
 };
 function runDiff() {
     return check_awaiter(this, void 0, void 0, function* () {
@@ -369,6 +370,7 @@ function runDiff() {
                     yield updateDiffComment(comment, body);
                     return messages.commentUpdated;
                 }
+                return messages.diffsUnchanged;
             }
             else {
                 yield createComment(body);
